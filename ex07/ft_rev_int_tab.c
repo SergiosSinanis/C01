c5f5b74b51a7bc5041c9ca9b963b33f76262a3a7
Sergios Sinanis <sergio.sinanis@gmail.com>
2024-03-07 20:45:49 +0100

    done

---
+++ ex07/ft_rev_int_tab.c
@@ -0,0 +1,16 @@
+void ft_rev_int_tab(int *tab, int size)
+{
+	int i;
+
+	i = 0;
+
+	int c;
+
+	while(i != size / 2)
+	{
+		c = tab[i];
+		tab[i] = tab[size -1 -i];
+		tab[size -1 -i] = c;
+		i++;
+	}
+}
